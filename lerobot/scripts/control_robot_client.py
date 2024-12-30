dceb49297bf8ad881ca30b5837f49f28d5005636
Josh Negreanu <joshua.negreanu@gmail.com>
2024-12-29 23:12:40 -0800

    remote record initial

--- lerobot/scripts/control_robot_client.py
+++ lerobot/scripts/control_robot_client.py
@@ -47,22 +47,22 @@ def remote_teleoperate(
     #if not robot.is_connected:
     #    robot.connect()
 
-    if teleop_time_s is None:
-        teleop_time_s = float("inf")
-    
-    # start timer
-    timestamp = 0
-    start_episode_t = time.perf_counter()
-
     # open socket for communication
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((ip, port))
 
     data = {}
     data['control_mode'] = 'remote_teleoperate'
-    data['fps'] = 30
+    data['teleop_time_s'] = teleop_time_s
+    data['fps'] = fps
     json_data = json.dumps(data)
     client_socket.send(json_data.encode().ljust(1024))
+
+    log_say(f"Teleoperate for {teleop_time_s} seconds", False)
+
+    # start timer
+    timestamp = 0
+    start_episode_t = time.perf_counter()
 
     pbar = tqdm.tqdm(range(teleop_time_s*fps))
     
@@ -79,8 +79,6 @@ def remote_teleoperate(
         busy_wait(1 / fps - dt_s)
 
         dt_s = time.perf_counter() - start_loop_t
-        #log_control_info(robot, dt_s, fps=fps)
-
         timestamp = time.perf_counter() - start_episode_t
     
     client_socket.close()
@@ -88,89 +86,79 @@ def remote_teleoperate(
 
 @safe_disconnect
 def remote_record(
-    robot: Robot,
-    repo_id : str,
-    root: str,
-    fps: int | None = None,
-    warmup_time_s=2,
-    episode_time_s=10,
-    reset_time_s=5,
-    num_episodes=50,
-    video=True,
-    run_compute_stats=True,
-    num_image_writer_processes=0,
-    num_image_writer_threads_per_camera=4,
-    force_override=False,
-    display_cameras=True,
-    play_sounds=True,
+    robot: Robot, 
+    fps: int, 
+    ip: str, 
+    port: int, 
+    warmup_time_s=2, 
+    episode_time_s=10, 
+    num_episodes=10
 ):
-    listener = None
-    events = None
-    policy = None
-    device = None
-    use_amp = None
 
-    if not robot.is_connected:
-        robot.connect()
+    #if not robot.is_connected:
+    #    robot.connect()
 
-    listener, events = init_keyboard_listener()
+    # open socket for communication
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ip, port))
 
-    # Execute a few seconds without recording to:
-    # 1. teleoperate the robot to move it in starting position if no policy provided,
-    # 2. give times to the robot devices to connect and start synchronizing,
-    # 3. place the cameras windows on screen
-    enable_teleoperation = policy is None
-    log_say("Warmup record", play_sounds)
-    warmup_record(robot, events, enable_teleoperation, warmup_time_s, display_cameras, fps)
+    data = {}
+    data['control_mode'] = 'remote_record'
+    data['fps'] = fps
+    data['warmup_time_s'] = warmup_time_s
+    data['episode_time_s'] = episode_time_s
+    data['num_episodes'] = num_episodes
+    json_data = json.dumps(data)
+    client_socket.send(json_data.encode().ljust(1024))
 
-    while True:
-        if dataset["num_episodes"] >= num_episodes:
-            break
+    log_say(f"Warmup record for {warmup_time_s} seconds", False)
+    timestamp = 0
+    start_episode_t = time.perf_counter()
 
-        episode_index = dataset["num_episodes"]
-        log_say(f"Recording episode {episode_index}", play_sounds)
-        record_episode(
-            dataset=dataset,
-            robot=robot,
-            events=events,
-            episode_time_s=episode_time_s,
-            display_cameras=display_cameras,
-            policy=policy,
-            device=device,
-            use_amp=use_amp,
-            fps=fps,
-        )
+    pbar = tqdm.tqdm(range(warmup_time_s*fps))
 
-        # Execute a few seconds without recording to give time to manually reset the environment
-        # Current code logic doesn't allow to teleoperate during this time.
-        # TODO(rcadene): add an option to enable teleoperation during reset
-        # Skip reset for the last episode to be recorded
-        if not events["stop_recording"] and (
-            (episode_index < num_episodes - 1) or events["rerecord_episode"]
-        ):
-            log_say("Reset the environment", play_sounds)
-            reset_environment(robot, events, reset_time_s)
+    # warmup
+    while timestamp < warmup_time_s:
+        pbar.update(1)
+        start_loop_t = time.perf_counter()
 
-        if events["rerecord_episode"]:
-            log_say("Re-record episode", play_sounds)
-            events["rerecord_episode"] = False
-            events["exit_early"] = False
-            delete_current_episode(dataset)
-            continue
+        #motor_array = robot.leader_arms["main"].read("Present_Position")
+        motor_array = np.array([-0.43945312, 133.94531, 179.82422, -18.984375, -1.9335938, 34.541016])
+        client_socket.sendall(motor_array)
 
-        # Increment by one dataset["current_episode_index"]
-        save_current_episode(dataset)
+        dt_s = time.perf_counter() - start_loop_t
+        busy_wait(1 / fps - dt_s)
 
-        if events["stop_recording"]:
-            break
+        dt_s = time.perf_counter() - start_loop_t
+        timestamp = time.perf_counter() - start_episode_t
 
-    log_say("Stop recording", play_sounds, blocking=True)
-    stop_recording(robot, listener, display_cameras)
+    curr_episode = 0
 
-    lerobot_dataset = create_lerobot_dataset(dataset, run_compute_stats, push_to_hub, tags, play_sounds)
+    while curr_episode < num_episodes:
 
-    log_say("Exiting", play_sounds)
-    return lerobot_dataset
+        episode_index = curr_episode
+        log_say(f"Recording episode {episode_index} for {episode_time_s} seconds", False)
+
+        pbar = tqdm.tqdm(range(episode_time_s*fps))
+
+        timestamp = 0
+        start_episode_t = time.perf_counter()
+
+        while timestamp < episode_time_s:
+            pbar.update(1)
+            start_loop_t = time.perf_counter()
+
+            #motor_array = robot.leader_arms["main"].read("Present_Position")
+            motor_array = np.array([-0.43945312, 133.94531, 179.82422, -18.984375, -1.9335938, 34.541016])
+            client_socket.sendall(motor_array)
+
+            dt_s = time.perf_counter() - start_loop_t
+            busy_wait(1 / fps - dt_s)
+
+            dt_s = time.perf_counter() - start_loop_t
+            timestamp = time.perf_counter() - start_episode_t
+    
+        curr_episode += 1
 
 
 if __name__ == "__main__":
@@ -215,95 +203,22 @@ if __name__ == "__main__":
         "--fps", type=none_or_int, default=None, help="Frames per second (set to None to disable)"
     )
     parser_record.add_argument(
-        "--root",
-        type=Path,
-        default="data",
-        help="Root directory where the dataset will be stored locally at '{root}/{repo_id}' (e.g. 'data/hf_username/dataset_name').",
-    )
-    parser_record.add_argument(
-        "--repo-id",
-        type=str,
-        default="lerobot/test",
-        help="Dataset identifier. By convention it should match '{hf_username}/{dataset_name}' (e.g. `lerobot/test`).",
-    )
-    parser_record.add_argument(
         "--warmup-time-s",
         type=int,
-        default=10,
+        default=2,
         help="Number of seconds before starting data collection. It allows the robot devices to warmup and synchronize.",
     )
     parser_record.add_argument(
         "--episode-time-s",
         type=int,
-        default=60,
+        default=5,
         help="Number of seconds for data recording for each episode.",
     )
     parser_record.add_argument(
-        "--reset-time-s",
-        type=int,
-        default=60,
-        help="Number of seconds for resetting the environment after each episode.",
-    )
-    parser_record.add_argument("--num-episodes", type=int, default=50, help="Number of episodes to record.")
-    parser_record.add_argument(
-        "--run-compute-stats",
-        type=int,
-        default=1,
-        help="By default, run the computation of the data statistics at the end of data collection. Compute intensive and not required to just replay an episode.",
+        "--ip", type=str, default=None, help="IP address of host remote socket"
     )
     parser_record.add_argument(
-        "--push-to-hub",
-        type=int,
-        default=1,
-        help="Upload dataset to Hugging Face hub.",
-    )
-    parser_record.add_argument(
-        "--tags",
-        type=str,
-        nargs="*",
-        help="Add tags to your dataset on the hub.",
-    )
-    parser_record.add_argument(
-        "--num-image-writer-processes",
-        type=int,
-        default=0,
-        help=(
-            "Number of subprocesses handling the saving of frames as PNGs. Set to 0 to use threads only; "
-            "set to ≥1 to use subprocesses, each using threads to write images. The best number of processes "
-            "and threads depends on your system. We recommend 4 threads per camera with 0 processes. "
-            "If fps is unstable, adjust the thread count. If still unstable, try using 1 or more subprocesses."
-        ),
-    )
-    parser_record.add_argument(
-        "--num-image-writer-threads-per-camera",
-        type=int,
-        default=4,
-        help=(
-            "Number of threads writing the frames as png images on disk, per camera. "
-            "Too many threads might cause unstable teleoperation fps due to main thread being blocked. "
-            "Not enough threads might cause low camera fps."
-        ),
-    )
-    parser_record.add_argument(
-        "--force-override",
-        type=int,
-        default=0,
-        help="By default, data recording is resumed. When set to 1, delete the local directory and start data recording from scratch.",
-    )
-    parser_record.add_argument(
-        "-p",
-        "--pretrained-policy-name-or-path",
-        type=str,
-        help=(
-            "Either the repo ID of a model hosted on the Hub or a path to a directory containing weights "
-            "saved using `Policy.save_pretrained`."
-        ),
-    )
-    parser_record.add_argument(
-        "--policy-overrides",
-        type=str,
-        nargs="*",
-        help="Any key=value arguments to override config values (use dots for.nested=overrides)",
+        "--port", type=int, default=None, help="Port address of host remote socket"
     )
 
     args = parser.parse_args()
--- lerobot/scripts/control_robot_server.py
+++ lerobot/scripts/control_robot_server.py
@@ -39,7 +39,8 @@ from lerobot.common.utils.utils import init_hydra_config, init_logging, log_say,
 def remote_teleoperate(
     robot: Robot, 
     fps: int, 
-    client_socket: socket,
+    teleop_time_s: float, 
+    client_socket: socket
 ):
     #if not robot.is_connected:
     #    robot.connect()
@@ -47,110 +48,74 @@ def remote_teleoperate(
     # start timer
     timestamp = 0
     start_episode_t = time.perf_counter()
+
+    log_say(f"Teleoperate for {teleop_time_s} seconds", True)
     
     # teleoperation loop
-    while True:
+    for _ in range(teleop_time_s*fps):
         start_loop_t = time.perf_counter()
 
         #motor_array = robot.leader_arms["main"].read("Present_Position")
         data = client_socket.recv(48)
-        if not data:
-            client_socket.close()
-            break
         
         motor_array = np.frombuffer(data, dtype=np.float32)
         
         #robot.follower_arms["main"].write("Goal_Position", motor_array)
 
         dt_s = time.perf_counter() - start_loop_t
-        log_control_info(robot, dt_s, fps=fps)
+        #log_control_info(robot, dt_s, fps=fps)
 
 
 @safe_disconnect
 def remote_record(
-    robot: Robot,
-    repo_id : str,
-    root: str,
-    fps: int | None = None,
-    warmup_time_s=2,
-    episode_time_s=10,
-    reset_time_s=5,
-    num_episodes=50,
-    video=True,
-    run_compute_stats=True,
-    num_image_writer_processes=0,
-    num_image_writer_threads_per_camera=4,
-    force_override=False,
-    display_cameras=True,
-    play_sounds=True,
+    robot: Robot, 
+    fps: int, 
+    warmup_time_s: float, 
+    episode_time_s: float, 
+    num_episodes: int
 ):
-    listener = None
-    events = None
-    policy = None
-    device = None
-    use_amp = None
 
-    if not robot.is_connected:
-        robot.connect()
+    #if not robot.is_connected:
+    #    robot.connect()
 
-    listener, events = init_keyboard_listener()
+    timestamp = 0
+    start_episode_t = time.perf_counter()
 
-    # Execute a few seconds without recording to:
-    # 1. teleoperate the robot to move it in starting position if no policy provided,
-    # 2. give times to the robot devices to connect and start synchronizing,
-    # 3. place the cameras windows on screen
-    enable_teleoperation = policy is None
-    log_say("Warmup record", play_sounds)
-    warmup_record(robot, events, enable_teleoperation, warmup_time_s, display_cameras, fps)
+    log_say(f"Warmup record for {warmup_time_s} seconds", True)
 
-    while True:
-        if dataset["num_episodes"] >= num_episodes:
-            break
+    # warmup
+    for _ in range(warmup_time_s*fps):
+        start_loop_t = time.perf_counter()
 
-        episode_index = dataset["num_episodes"]
-        log_say(f"Recording episode {episode_index}", play_sounds)
-        record_episode(
-            dataset=dataset,
-            robot=robot,
-            events=events,
-            episode_time_s=episode_time_s,
-            display_cameras=display_cameras,
-            policy=policy,
-            device=device,
-            use_amp=use_amp,
-            fps=fps,
-        )
+        #motor_array = robot.leader_arms["main"].read("Present_Position")
+        data = client_socket.recv(48)
+        
+        motor_array = np.frombuffer(data, dtype=np.float32)
+        #robot.follower_arms["main"].write("Goal_Position", motor_array)
 
-        # Execute a few seconds without recording to give time to manually reset the environment
-        # Current code logic doesn't allow to teleoperate during this time.
-        # TODO(rcadene): add an option to enable teleoperation during reset
-        # Skip reset for the last episode to be recorded
-        if not events["stop_recording"] and (
-            (episode_index < num_episodes - 1) or events["rerecord_episode"]
-        ):
-            log_say("Reset the environment", play_sounds)
-            reset_environment(robot, events, reset_time_s)
+        dt_s = time.perf_counter() - start_loop_t
+        #log_control_info(robot, dt_s, fps=fps)
+    
+    curr_episode = 0
 
-        if events["rerecord_episode"]:
-            log_say("Re-record episode", play_sounds)
-            events["rerecord_episode"] = False
-            events["exit_early"] = False
-            delete_current_episode(dataset)
-            continue
+    while curr_episode < num_episodes:
 
-        # Increment by one dataset["current_episode_index"]
-        save_current_episode(dataset)
+        episode_index = curr_episode
+        log_say(f"Recording episode {episode_index} for {episode_time_s} seconds", True)
 
-        if events["stop_recording"]:
-            break
+        for _ in range(episode_time_s*fps):
+            start_loop_t = time.perf_counter()
 
-    log_say("Stop recording", play_sounds, blocking=True)
-    stop_recording(robot, listener, display_cameras)
+            #motor_array = robot.leader_arms["main"].read("Present_Position")
+            data = client_socket.recv(48)
+            
+            motor_array = np.frombuffer(data, dtype=np.float32)
+            #robot.follower_arms["main"].write("Goal_Position", motor_array)
 
-    lerobot_dataset = create_lerobot_dataset(dataset, run_compute_stats, push_to_hub, tags, play_sounds)
+            dt_s = time.perf_counter() - start_loop_t
+            #log_control_info(robot, dt_s, fps=fps)
 
-    log_say("Exiting", play_sounds)
-    return lerobot_dataset
+        curr_episode += 1
 
 
 if __name__ == "__main__":
@@ -176,10 +141,14 @@ if __name__ == "__main__":
         fps = json_data['fps']
 
         if control_mode == 'remote_teleoperate':
-            remote_teleoperate(robot, fps, client_socket)
+            teleop_time_s = json_data['teleop_time_s']
+            remote_teleoperate(robot, teleop_time_s, fps, client_socket)
 
         elif control_mode == "remote_record":
-            remote_record(robot, ...)
+            warmup_time_s = json_data['warmup_time_s']
+            episode_time_s = json_data['episode_time_s']
+            num_episodes = json_data['num_episodes']
+            remote_record(robot, fps, warmup_time_s, episode_time_s, num_episodes)
 
     if robot.is_connected:
         # Disconnect manually to avoid a "Core dump" during process
