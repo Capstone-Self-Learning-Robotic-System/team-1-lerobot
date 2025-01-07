d7a4492fef523b5df040f41ba911fb4db9c1fb53
Josh Negreanu <33441103+JoshNegreanu@users.noreply.github.com>
2025-01-07 09:34:45 -0800

    busy wait change

--- lerobot/common/robot_devices/utils.py
+++ lerobot/common/robot_devices/utils.py
@@ -2,18 +2,10 @@ import platform
 import time
 
 
-def busy_wait(seconds):
-    if platform.system() == "Darwin":
-        # On Mac, `time.sleep` is not accurate and we need to use this while loop trick,
-        # but it consumes CPU cycles.
-        # TODO(rcadene): find an alternative: from python 11, time.sleep is precise
-        end_time = time.perf_counter() + seconds
-        while time.perf_counter() < end_time:
-            pass
-    else:
-        # On Linux time.sleep is accurate
-        if seconds > 0:
-            time.sleep(seconds)
+def busy_wait(dt):   
+    current_time = time.time()
+    while (time.time() < current_time+dt):
+        pass
 
 
 def safe_disconnect(func):
