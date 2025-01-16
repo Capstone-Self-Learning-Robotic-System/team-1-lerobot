1d9fdbcc9b5ef01246f0169d64ab226b4a360613
Joshua Brown <josh.bikedude.419@gmail.com>
2025-01-16 15:31:14 -0800

    test servers working

--- lerobot/scripts/test_client.py
+++ lerobot/scripts/test_client.py
@@ -7,7 +7,7 @@ import pickle
 import time
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect(("localhost", 50068))
+server.connect(("50.39.109.27", 50064))
 
 buffer = b''
 start = time.perf_counter()
