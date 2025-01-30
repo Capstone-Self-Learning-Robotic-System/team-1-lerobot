7ffb17601605ce995374d85e3823386f325eaaaf
Joshua Brown <josh.bikedude.419@gmail.com>
2025-01-30 14:54:35 -0800

    Undo LeRobot Changes (#5)
    * changes should be reverted
    
    * update config
    
    ---------
    
    Co-authored-by: lelabpc <leemail@leemail.com>

--- lerobot/common/robot_devices/robots/manipulator.py
+++ lerobot/common/robot_devices/robots/manipulator.py
@@ -81,7 +81,7 @@ class ManipulatorRobotConfig:
         super().__setattr__(prop, val)
 
     def __post_init__(self):
-        if self.robot_type not in ["koch", "koch_bimanual", "koch_leader", "koch_follower", "aloha", "so100", "moss"]:
+        if self.robot_type not in ["koch", "koch_bimanual", "aloha", "so100", "moss"]:
             raise ValueError(f"Provided robot type ({self.robot_type}) is not supported.")
 
 
@@ -300,7 +300,7 @@ class ManipulatorRobot:
             print(f"Connecting {name} leader arm.")
             self.leader_arms[name].connect()
 
-        if self.robot_type in ["koch", "koch_bimanual", "koch_leader", "koch_follower", "aloha"]:
+        if self.robot_type in ["koch", "koch_bimanual", "aloha"]:
             from lerobot.common.robot_devices.motors.dynamixel import TorqueMode
         elif self.robot_type in ["so100", "moss"]:
             from lerobot.common.robot_devices.motors.feetech import TorqueMode
@@ -315,7 +315,7 @@ class ManipulatorRobot:
         self.activate_calibration()
 
         # Set robot preset (e.g. torque in leader gripper for Koch v1.1)
-        if self.robot_type in ["koch", "koch_bimanual", "koch_leader", "koch_follower"]:
+        if self.robot_type in ["koch", "koch_bimanual"]:
             self.set_koch_robot_preset()
         elif self.robot_type == "aloha":
             self.set_aloha_robot_preset()
@@ -328,7 +328,7 @@ class ManipulatorRobot:
             self.follower_arms[name].write("Torque_Enable", 1)
 
         if self.config.gripper_open_degree is not None:
-            if self.robot_type not in ["koch", "koch_bimanual", "koch_leader", "koch_follower"]:
+            if self.robot_type not in ["koch", "koch_bimanual"]:
                 raise NotImplementedError(
                     f"{self.robot_type} does not support position AND current control in the handle, which is require to set the gripper open."
                 )
